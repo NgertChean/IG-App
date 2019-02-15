55af9627966f1e753b46d8f639b9e537c857f862
Ngert Chean <NgertChean@outlook.com>
2019-02-15 12:24:39 +0700

    Upgrade RN version to 0.58.4

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import { AppRegistry } from 'react-native';
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
+import 'babel-polyfill';
 
 console.disableYellowBox = true;
-AppRegistry.registerComponent('IG', () => App);
+AppRegistry.registerComponent(appName, () => App);
